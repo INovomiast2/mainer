5fd8df7b2a952bb03e7baa506dda034da6c38766
INovomiast2 <idimnovdie1602@protonmail.com>
2023-10-02 08:58:37 -0400

    Npe

--- utils/install.sh
+++ utils/install.sh
@@ -17,15 +17,15 @@ git clone https://github.com/INovomiast2/mainer.git ~/.mainer
 echo "${GREEN}Mainer downloaded!${NC}"
 echo "${GREEN}Installing Mainer...${NC}"
 # Check if python3 is installed
-if ! command -v python &> /dev/null; then
-  echo "${RED}Python3 is not installed!${NC}"
-  exit 1
-fi
-# Check if pip3 is installed
-if ! command -v pip &> /dev/null; then
-  echo "${RED}Pip3 is not installed!${NC}"
-  exit 1
-fi
+# if ! command -v python &> /dev/null; then
+#   echo "${RED}Python3 is not installed!${NC}"
+#   exit 1
+# fi
+# # Check if pip3 is installed
+# if ! command -v pip &> /dev/null; then
+#   echo "${RED}Pip3 is not installed!${NC}"
+#   exit 1
+# fi
 # Install all the python3 dependencies
 pip3 install -r ~/.mainer/requirements.txt
 # When all the requirements are installed, add alias to .zshrc
